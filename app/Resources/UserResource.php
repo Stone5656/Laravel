dbaf8e8b532fafab5a576f910241e838ec736d6b
jo <jo.yasuda.dev@gmail.com>
2025-10-25 11:19:39 +0900

    DTOをResouceに修正

---
+++ app/Resources/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'roles' => $this->roles,
+            'is_stream' => (bool)$this->is_stream,
+            'bio' => $this->bio,
+            'profile_image_path' => $this->profile_image_path,
+            'cover_image_path' => $this->cover_image_path,
+            'channel_name' => $this->channel_name,
+            'created_at' => optional($this->created_at)->toISOString(),
+            'updated_at' => optional($this->updated_at)->toISOString(),
+        ];
+    }
+}
